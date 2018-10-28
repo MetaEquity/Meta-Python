1feafe16ef52a393af2f0de78c24efc064a4f43a
romasalah <roma.salah@gmail.com>
2018-10-28 20:45:49 +0100

    added DDR getters and setters
    added DDR getters and setters & a direct for block reward after considering DDR(dynamic distribution ratio)+temporal decay and most recent price ROC (rate of change).

--- DDR.py
+++ DDR.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct 14 22:08:45 2018
-
-@author: Omar Salah
-"""
+#get_Height is a method of Neo-python library.
+#get_price is a method in metaequity oracle.
 import numpy as np
 
 class dynamic_distributer:
-    def __init__(self,token_id,initOwnerShare,blocktime):
+    def __init__(self,token_id,initOwnerShare,blocktime,ROCinetrvaldays):
         #this must be limited to Master-admin privilages
         self.token_id=token_id
         self.price= get_price(token_id)
@@ -16,19 +12,20 @@ class dynamic_distributer:
         self.rapid=1
         self.slow=1
         self.blocktime=blocktime
+        self.ROCinetrval=ROCinetrvaldays
         
-    def get_pricefactor(self,timedays):
-         self.ROC=get_change(self.token_id,timedays)
+    def get_pricefactor(self):
+         self.ROC=self.get_change()
          y=self.ROC
          k=self.rapid
          s=self.slow
          self.pricefactor= ((-4)/(1+np.exp(((y^2)/(-4*s))))+(1/(0.5+(k*(y^2))))-2)
          return self.pricefactor # only an output, not used for calculations
 
-    def get_change(self,timedays):
+    def get_change(self):
          cur_block=Get_Height(self.token_id)
          #blocktime should be replaced by the average
-         n=(timedays*24*60*60)/self.blocktime
+         n=(self.ROCinterval*24*60*60)/self.blocktime
          #number of blocks to go back
          self.ROC=(get_price(self.token_id,cur_block)- get_price(self.token_id,cur_block-n))
          return self.ROC
@@ -40,7 +37,7 @@ class dynamic_distributer:
         if  0.5 < slow < 8:
             self.slow=slow
     
-    def time_decay(self,halfduration,maxSupply,slowcoef,rapidcoef):
+    def set_decay(self,halfduration,maxSupply,slowcoef,rapidcoef):
         #a defining function for a static decay.
         #for the first time definition of the decay.
         # needs adaptation for realtime dynamic changes
@@ -54,3 +51,39 @@ class dynamic_distributer:
         x=GetHeight(self.token_id)
         self.blockreward=-((np.ln(2)*a*(( -1*(4/(np.exp(((y^2)/(4*s)))+1)))+(1/((k*(y^2))+0.5))-2)*2^(x*(( -1*(4/(np.exp(((y^2)/(4*s)))+1))+(1/((k*(y^2))+0.5))-2))))/c)
         return self.blockreward
+    
+    def get_reward(self,Height):
+        x=Height
+        y=self.ROC
+        a=self.max
+        c=self.duration*2
+        s=self.slow
+        k=self.rapid
+        self.blockreward=-((np.ln(2)*a*(( -1*(4/(np.exp(((y^2)/(4*s)))+1)))+(1/((k*(y^2))+0.5))-2)*2^(x*(( -1*(4/(np.exp(((y^2)/(4*s)))+1))+(1/((k*(y^2))+0.5))-2))))/c)
+        return self.blockreward
+    
+    def get_DDR(self):
+        #Only calculates current ratio
+        self.ROC=self.get_change()
+        b=self.DDRmin
+        c=self.DDRrange
+        k=self.DDRslope
+        self.owner=b+(c/(1+np.exp(-k*self.ROC)))
+        self.public=100-self.owner
+        
+    def set_DDR(self, minimum,rang,slope):
+        if 20 <= minimum <=80:
+            self.DDRmin=minimum
+        if 20 <= rang <=80:
+            self.DDRrange=rang
+        if 0.05 <= slope <= 10:
+            self.DDRslope=slope
+            
+    def get_DDR_reward(self,ownerFlag):
+        H=get_Height(self.token_id)
+        ddr=self.get_DDR()
+        if ownerFlag==1:
+           return self.get_reward(H)*(ddr/100)
+        if not ownerFlag==1:
+            return self.get_reward(H)*(1-(ddr/100))
+            
